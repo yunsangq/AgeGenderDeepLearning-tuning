bd981913c9ddd2ebb089a0faa823d2d6fdf5a248
yunsangq <yunsangq@gmail.com>
2017-05-24 15:59:38 +0900

    graph

--- gender_net_definitions/guess.py
+++ gender_net_definitions/guess.py
@@ -7,20 +7,21 @@ GENDER_LIST = ['M', 'F']
 caffe.set_mode_gpu()
 caffe.set_device(0)
 
+fold_number = 4
 model_file = './deploy.prototxt'
-trained = './model_fold_0/caffenet_train_iter_50000.caffemodel'
+trained = './model_fold_'+str(fold_number)+'/caffenet_train_iter_50000.caffemodel'
 guess = '../example/example_image0.jpg'
 
 face_list = utils.faceDetector(guess, 'face')
 
 blob = caffe.proto.caffe_pb2.BlobProto()
-data = open('../Folds/lmdb/Test_fold_is_0/mean.binaryproto', 'rb').read()
+data = open('../Folds/lmdb/Test_fold_is_'+str(fold_number)+'/mean.binaryproto', 'rb').read()
 blob.ParseFromString(data)
 arr = np.array(caffe.io.blobproto_to_array(blob))
-np.save('./model_fold_0/mean.npy', arr[0])
+np.save('./model_fold_'+str(fold_number)+'/mean.npy', arr[0])
 
 net = caffe.Classifier(model_file, trained,
-                       mean=np.load('./model_fold_0/mean.npy').mean(1).mean(1),
+                       mean=np.load('./model_fold_'+str(fold_number)+'/mean.npy').mean(1).mean(1),
                        channel_swap=(2, 1, 0),
                        raw_scale=255,
                        image_dims=(227, 227))
