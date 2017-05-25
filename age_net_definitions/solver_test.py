e0b1b8db921986858d47cbcd237ade4079051a4a
yunsangq <yunsangq@gmail.com>
2017-05-25 21:07:08 +0900

    backup

--- age_net_definitions/solver_test.py
+++ age_net_definitions/solver_test.py
@@ -1,81 +1,88 @@
 import caffe
 import cv2
-from PIL import Image
-import matplotlib
+import matplotlib.pyplot as plt
 import lmdb
 import numpy as np
+from collections import defaultdict
+import sys
+from PIL import Image, ImageOps
 
 MODEL_FILE = './deploy.prototxt'
 TRAIN_FILE = './model_fold_0/caffenet_train_iter_50000.caffemodel'
 DB_PATH = '/home/k/PycharmProjects/AgeGenderDeepLearning-master/Folds/lmdb/Test_fold_is_0/age_test_lmdb'
-resize_h = 227
-resize_w = 227
+fold_number = 0
 
-def test():
-    net = caffe.Net(MODEL_FILE, TRAIN_FILE, caffe.TEST)
-    caffe.set_mode_gpu()
 
-    lmdb_env = lmdb.open(DB_PATH)
+def flat_shape(x):
+    "Returns x without singleton dimension, eg: (1,28,28) -> (28,28)"
+    return x.reshape(filter(lambda s: s > 1, x.shape))
+
+
+def lmdb_reader(fpath):
+    lmdb_env = lmdb.open(fpath)
     lmdb_txn = lmdb_env.begin()
     lmdb_cursor = lmdb_txn.cursor()
-    count = 0
-    correct = 0
+
     for key, value in lmdb_cursor:
-        count = count + 1
         datum = caffe.proto.caffe_pb2.Datum()
         datum.ParseFromString(value)
         label = int(datum.label)
-        image = caffe.io.datum_to_array(datum)
-        #image = image[0, :, :]
-        height, width = image.shape
-        new_image = cv2.resize(image, (resize_w, resize_h))
-        transf = np.zeros((1, resize_h, resize_w), dtype=np.uint8)
-        transf[0, :, :] = new_image
-
-        image = image.astype(np.uint8)
-        out = net.forward_all(data=np.asarray([image]))
-        predicted_label = out['prob'][0].argmax(axis=0)
-        if label == predicted_label:
-            correct = correct + 1
-        # print label
-        # print np.shape(image)
-        # break
-    print 'count: %s, correct: %s, rate: %f' % (count, correct, float(correct) / count)
+        image = caffe.io.datum_to_array(datum).astype(np.uint8)
+        yield (key, flat_shape(image), label)
 
 
-def main():
-    # net = caffe.Classifier(
-    #     caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt',
-    #     caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel',
-    #     image_dims=(256, 256))
-    # net.set_phase_test()
-    # net.set_mode_cpu()
-    # net.set_mean('data', np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy'))
-    # net.set_raw_scale('data', 255)
-    # net.set_channel_swap('data', (2, 1, 0))
-    # scores = net.predict([caffe.io.load_image("/home/john/Pictures/dog.jpg")])
-    # print 'scores: ', scores
-    return
+if __name__ == "__main__":
+    # Extract mean from the mean image file
+    mean_blobproto_new = caffe.proto.caffe_pb2.BlobProto()
+    f = open('../Folds/lmdb/Test_fold_is_'+str(fold_number)+'/mean.binaryproto', 'rb')
+    mean_blobproto_new.ParseFromString(f.read())
+    mean_image = caffe.io.blobproto_to_array(mean_blobproto_new)
+    f.close()
 
-def testLMDB():
-    lmdb_env = lmdb.open('/home/john/caffe/examples/myimagenet/ilsvrc12_train_lmdb')
-    lmdb_txn = lmdb_env.begin()
-    lmdb_cursor = lmdb_txn.cursor()
     count = 0
     correct = 0
-    for key, value in lmdb_cursor:
-        count = count + 1
-        datum = caffe.proto.caffe_pb2.Datum()
-        datum.ParseFromString(value)
-        image = caffe.io.datum_to_array(datum)
-        image = image.astype(np.uint8)
-        # print np.shape(image)
-        # break
-    print 'end:', count
-    return
+    matrix = defaultdict(int)  # (real,pred) -> int
+    labels_set = set()
 
-if __name__ == '__main__':
-    print 'start'
-    # main()
-    #testLMDB()
-    test()
+    # CNN reconstruction and loading the trained weights
+    net = caffe.Net(MODEL_FILE, TRAIN_FILE, caffe.TEST)
+    caffe.set_mode_gpu()
+
+    reader = lmdb_reader(DB_PATH)
+
+    for i, image, label in reader:
+        image_caffe = image.reshape(1, *image.shape)
+        image_caffe = np.asarray(image_caffe) - mean_image
+        img_array = image_caffe[0].transpose((1, 2, 0))  # (c,h,w)->(h,w,c)
+        img = Image.fromarray(np.uint8(img_array))
+        img = img.resize((227, 227))
+        img_array = np.asarray(img)
+        plt.imshow(img_array)
+        plt.show()
+        img_array = img_array.transpose((2, 0, 1))  # (h,w,c) -> (c,h,w)
+
+        img_array = img_array.reshape(1, *img_array.shape)
+        out = net.forward_all(data=img_array)
+        plabel = int(out['prob'][0].argmax(axis=0))
+
+        count += 1
+        iscorrect = label == plabel
+        correct += (1 if iscorrect else 0)
+        matrix[(label, plabel)] += 1
+        labels_set.update([label, plabel])
+
+        if not iscorrect:
+            print("\rError: i=%s, expected %i but predicted %i" \
+                  % (i, label, plabel))
+
+        sys.stdout.write("\rAccuracy: %.1f%%" % (100. * correct / count))
+        sys.stdout.flush()
+
+    print(", %i/%i corrects" % (correct, count))
+
+    print ""
+    print "Confusion matrix:"
+    print "(r , p) | count"
+    for l in labels_set:
+        for pl in labels_set:
+            print "(%i , %i) | %i" % (l, pl, matrix[(l, pl)])
