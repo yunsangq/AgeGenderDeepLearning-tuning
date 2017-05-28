f1ed37a6f6c8303ea1957cbad0a03a3524c9a812
yunsangq <yunsangq@gmail.com>
2017-05-28 18:48:19 +0900

    backup

--- gender_resnet_definitions/test.py
+++ gender_resnet_definitions/test.py
@@ -4,13 +4,15 @@ import numpy as np
 import lmdb
 from collections import defaultdict
 
+GENDER_LIST = ['M', 'F']
+
 caffe.set_mode_gpu()
 caffe.set_device(0)
 
 fold_number = 0
-model_file = './50-model_fold_'+str(fold_number)+'/deploy.prototxt'
-trained = './50-model_fold_'+str(fold_number)+'/pre-resnet-50_iter_200000.caffemodel'
-txt_file = '../Folds/train_val_txt_files_per_fold/test_fold_is_'+str(fold_number)+'/age_test.txt'
+model_file = './34-model_fold_0/deploy.prototxt'
+trained = './34-model_fold_'+str(fold_number)+'/pre-resnet-34_iter_70000.caffemodel'
+txt_file = '../Folds/train_val_txt_files_per_fold/test_fold_is_'+str(fold_number)+'/gender_test.txt'
 aligned = '../../models/age-gender/aligned/'
 
 fname_list = []
@@ -25,17 +27,17 @@ blob = caffe.proto.caffe_pb2.BlobProto()
 data = open('../Folds/lmdb/Test_fold_is_'+str(fold_number)+'/mean.binaryproto', 'rb').read()
 blob.ParseFromString(data)
 arr = np.array(caffe.io.blobproto_to_array(blob))
-np.save('./50-model_fold_'+str(fold_number)+'/mean.npy', arr[0])
+np.save('./34-model_fold_'+str(fold_number)+'/mean.npy', arr[0])
 
 net = caffe.Classifier(model_file, trained,
-                       mean=np.load('./50-model_fold_'+str(fold_number)+'/mean.npy').mean(1).mean(1),
+                       mean=np.load('./34-model_fold_'+str(fold_number)+'/mean.npy').mean(1).mean(1),
                        channel_swap=(2, 1, 0),
-                       raw_scale=255)
+                       raw_scale=255,
+                       image_dims=(224, 224))
 
 if __name__ == "__main__":
     count = 0.
     correct = 0.
-    one_off = 0.
     matrix = defaultdict(int) # (real,pred) -> int
     labels_set = set()
 
@@ -44,27 +46,20 @@ if __name__ == "__main__":
         # plt.imshow(image)
         # plt.show()
         label = label_list[idx]
-
         prediction = net.predict([image])
         plabel = prediction[0].argmax()
 
         count = count + 1
         iscorrect = label == plabel
         correct = correct + (1 if iscorrect else 0)
-
-        if plabel == label or plabel == label-1 or plabel == label+1:
-            one_off += 1
-
         matrix[(label, plabel)] += 1
         labels_set.update([label, plabel])
 
         if count % 100 == 0:
             print('Exact -> %i/%i, acc: %f' % (correct, count, correct/count))
-            print('1-off -> %i/%i, acc: %f' % (one_off, count, one_off/count))
 
     print("Fold: %i" % (fold_number))
     print("Exact Accuracy: %f%%" % (correct/count))
-    print("1-off Accuracy: %f%%" % (one_off / count))
     print(str(correct) + " out of " + str(count) + " were classified correctly")
 
     print ""
